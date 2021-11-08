6117400188364c7a040f25f5636534a7727812f2
MING <faith_my@sina.com>
2021-11-08 20:57:41 +0800

    bscs

---
+++ src/pages/Index.vue
@@ -0,0 +1,142 @@
+<template>
+  <Layout>
+    <!-- Page Header-->
+    <header
+      class="masthead"
+      :style="{
+        backgroundImage: `url(${GRIDSOME_API_URL+general.cover.url})`,
+      }"
+    >
+      <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-md-10 col-lg-8 col-xl-7">
+            <div class="site-heading">
+              <h1>{{ general.title }}</h1>
+              <span class="subheading">{{ general.subtitle }}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- Main Content-->
+    <div class="container px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
+          <!-- Post preview-->
+          <!-- <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                Man must explore, and this is exploration at its greatest
+              </h2>
+              <h3 class="post-subtitle">
+                Problems look mighty small from 150 miles up
+              </h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#!">Start Bootstrap</a>
+              on September 24, 2021
+            </p>
+          </div> -->
+          <!-- Divider-->
+          <!-- <hr class="my-4" /> -->
+          <!-- Post preview-->
+          <div
+            class="post-preview"
+            v-for="edge in $page.posts.edges"
+            :key="edge.node.id"
+          >
+            <g-link :to="'/posts/' + edge.node.id">
+              <h2 class="post-title">
+                {{ edge.node.title }}
+              </h2>
+            </g-link>
+            <p class="post-meta">
+              Posted by {{ edge.node.author }}
+              <!-- <a href="#!">Start Bootstrap</a> -->
+              on {{ edge.node.created_at }}
+            </p>
+            <p>
+              <span v-for="tag in edge.node.tags" :key="tag">
+                <g-link :to="'/tag/' + tag.id">{{ tag.title }}</g-link>
+                &nbsp;&nbsp;
+              </span>
+            </p>
+            <hr class="my-4" />
+          </div>
+          <!-- Pager-->
+          <!-- <div class="d-flex justify-content-end mb-4">
+            <a class="btn btn-primary text-uppercase" href="#!"
+              >Older Posts →</a
+            >
+          </div> -->
+          <Pager class="pages_wrap" :info="$page.posts.pageInfo"></Pager>
+        </div>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query($page:Int){
+  posts: allStrapiPost(perPage:3, page:$page) @paginate{
+    pageInfo{
+      totalPages
+      currentPage
+    }
+    edges{
+      node{
+        id
+        title
+        created_at
+        author
+        tags{
+          id
+          title
+        }
+      }
+    }
+  }
+
+  general:allStrapiGeneral{
+    edges{
+      node{
+        id
+        title
+        subtitle
+        cover{
+          url
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import { Pager } from "gridsome";
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+  name: "HomePage",
+  components: {
+    Pager,
+  },
+  computed: {
+    general() {
+      return this.$page.general.edges[0].node;
+    },
+  },
+};
+</script>
+
+<style>
+.pages_wrap {
+  width: 100%;
+  height: auto;
+}
+.pages_wrap a {
+  margin-right: 10px;
+}
+</style>
